09d58d6bf26b69fffab5a2ecd83c010aa0e91e40
colorplease <alistairvtran@gmail.com>
2021-01-01 18:32:08 -0800

    Basic Movement, Aiming, and Shooting  in 2D

---
+++ Dani GameJam/Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        //Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+}
